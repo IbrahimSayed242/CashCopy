89836c9904db081ef0f6354ce2aa77eb2384ea56
IbrahimSayed242 <hema3sh@gmail.com>
2023-12-06 13:31:34 +0200

    v2.0

--- src/components/sections/Swip.vue
+++ src/components/sections/Swip.vue
@@ -26,11 +26,11 @@
           </button>
         <div class="cont">
           <h2 class="text-white">
-            كاش باك 500
+            
           </h2>
         </div>
         <img
-        src="https://s6.imgcdn.dev/VaTWh.webp" />
+        src="/images/evest.webp" />
         </swiper-slide
 
     ><swiper-slide
@@ -39,60 +39,18 @@
            فتح حساب
           </button>
        <img
-        src="https://swiperjs.com/demos/images/nature-2.jpg" /></swiper-slide
+        src="/images/exness.webp" /></swiper-slide
     ><swiper-slide
       >
       <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
            فتح حساب
           </button>
       <img
-        src="https://swiperjs.com/demos/images/nature-3.jpg" /></swiper-slide
-    ><swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
-           فتح حساب
-          </button>
-      
-      <img
-        src="https://swiperjs.com/demos/images/nature-4.jpg" /></swiper-slide
-    ><swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
-           فتح حساب
-          </button>
-      <img
-        src="https://swiperjs.com/demos/images/nature-5.jpg" /></swiper-slide
-    ><swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
-           فتح حساب
-          </button>
-      <img
-        src="https://swiperjs.com/demos/images/nature-6.jpg" /></swiper-slide
-    ><swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-           فتح حساب
-          </button>
-      <img
-        src="https://swiperjs.com/demos/images/nature-7.jpg" /></swiper-slide
-    ><swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-           فتح حساب
-          </button>
-      <img
-        src="https://swiperjs.com/demos/images/nature-8.jpg" /></swiper-slide
+        src="/images/ingot.webp" /></swiper-slide
     >
-    <swiper-slide
-      >
-      <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
-           فتح حساب
-          </button>
-      <img src="https://swiperjs.com/demos/images/nature-9.jpg"
-    />
-  </swiper-slide>
   </swiper>
+
+  
 </template>
 <script>
   // Import Swiper Vue.js components
