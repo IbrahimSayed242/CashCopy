5ebed9447e33a8b98c32a5a33425b1322694e780
Mena Heero <menaheero@gmail.com>
2023-12-07 18:51:45 +0200

    update 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,14 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue()]
+  integrations: [tailwind(), react(), vue()],
+  outDir: "../django/templates/astro/",
+  publicDir: "../django/media/",
+  trailingSlash: "always",
+  compressHTML: process.env.NODE_ENV === "production",
+  build: {
+    format: "file",
+    inlineStylesheets: "auto",
+    assets: "static",
+  },
 });
