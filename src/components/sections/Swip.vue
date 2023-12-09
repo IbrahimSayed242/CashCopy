0a7de23833a3433c0064d98af928c3ca153ef102
IbrahimSayed242 <hema3sh@gmail.com>
2023-12-09 16:03:46 +0200

    v4.1

--- src/components/sections/Swip.vue
+++ src/components/sections/Swip.vue
@@ -41,7 +41,7 @@
           </h2>
         </div>
         <img
-        src="/images/evest.webp" />
+        src="https://s6.imgcdn.dev/V2F6O.webp" />
         </swiper-slide
 
     ><swiper-slide
@@ -50,14 +50,15 @@
            فتح حساب
           </button>
        <img
-        src="/images/exness.webp" /></swiper-slide
+        src="https://s6.imgcdn.dev/V2Hln.webp
+" /></swiper-slide
     ><swiper-slide
       >
       <button class="bt text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
            فتح حساب
           </button>
       <img
-        src="/images/ingot.webp" /></swiper-slide
+        src="https://s6.imgcdn.dev/V2XVg.webp" /></swiper-slide
     >
   </swiper>
 
