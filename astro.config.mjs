2454d8098af1c30d319b4a3c53c13b029b57e26f
IbrahimSayed242 <hema3sh@gmail.com>
2023-12-09 10:56:39 +0200

    v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,13 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), vue()],
-  outDir: "../django/templates/astro/",
-  publicDir: "../django/media/",
-  trailingSlash: "always",
-  compressHTML: process.env.NODE_ENV === "production",
-  build: {
-    format: "file",
-    inlineStylesheets: "auto",
-    assets: "static",
-  },
+  // outDir: "../django/templates/astro/",
+  // publicDir: "../django/media/",
+  // trailingSlash: "always",
+  // compressHTML: process.env.NODE_ENV === "production",
+  // build: {
+  //   format: "file",
+  //   inlineStylesheets: "auto",
+  //   assets: "static",
+  // },
 });
