88be0c69b04a36ce5db91fbbd1f50c9d9ebaf066
Mena Heero <menaheero@gmail.com>
2023-12-21 12:56:51 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import AstroPWA from '@vite-pwa/astro'
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue()],
+  integrations: [tailwind(),
+     react(),
+      vue(),
+      AstroPWA({
+      }),
+    ],
+    
+
   outDir: "../astrobuild/",
   // publicDir: "../astrobuild/media/",
   // trailingSlash: "always",
