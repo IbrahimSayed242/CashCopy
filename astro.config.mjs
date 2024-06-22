d0927e689d346e1b185ae2efc4b689edc3c29db2
Mena Heero <menaheero@gmail.com>
2024-06-22 15:32:51 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,4 +17,5 @@ export default defineConfig({
   //   assets: "static",
   // },
   output: "server",
+  adapter: vercel(),
 });
