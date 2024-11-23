e0a5375f236bb3d271aa5c9579a85492cf10794f
Hisham <hisham4a4d4m@gmail.com>
2024-11-23 16:03:40 +0200

    version 6.0 add loading and fix comparsion page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../public/css/loading.css";
+
+const Loading = () => {
+  return (
+    <div className="container-load">
+      <div className="load">
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+      </div>
+    </div>
+  );
+};
+export default Loading;
